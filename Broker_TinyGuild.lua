cd558f1ab3608321a0caf1337e178d874e31376f
Pinta365 <hello@pinta.land>
2025-01-03 15:53:28 +0100

    fix for unguildeds

--- Broker_TinyGuild.lua
+++ Broker_TinyGuild.lua
@@ -387,17 +387,25 @@ local function OnEvent(self, event, ...)
         InitBroker()
     elseif event == "GUILD_MOTD" then
         updateGMOTD()
+    elseif event == "PLAYER_ENTERING_WORLD" then
+        --Dirty fix for unguilded, IsInGuild() is not up to speed right away.
+        C_Timer.NewTimer(4, function()
+            if not IsInGuild() then
+                AddonTable.guildName = "No Guild"
+                AddonTable.BrokerTinyGuild.text = "No Guild"
+            end
+        end)
     elseif event == "PLAYER_GUILD_UPDATE" or 
             (IsInGuild() and (event == "GUILD_ROSTER_UPDATE" or
                  event == "GUILD_RANKS_UPDATE")) then
         updateGuildOnline()
-
     end
 end
 
 local f = CreateFrame("Frame", "BrokerTinyGuild")
 f:RegisterEvent("ADDON_LOADED")
 f:RegisterEvent("GUILD_MOTD")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:RegisterEvent("GUILD_ROSTER_UPDATE")
 f:RegisterEvent("GUILD_RANKS_UPDATE")
 f:RegisterEvent("PLAYER_GUILD_UPDATE")
